5501bf7835c4d38f7cababdb564628588144c2a3
Isaac Eduardo Velez Ortega <eduardo_vsiete@yahoo.com>
2023-06-06 13:52:46 -0600

    Adicion de la funcionalidad par adesencriptar un texto

--- index.js
+++ index.js
@@ -26,5 +26,29 @@ function encriptar () {
 
 }
 
-                    
-                    
+function desencriptar () {
+   let texto = document.getElementById("texto").value;
+   let tituloMensaje = document.getElementById("titulo-mensaje");
+   let parrafo = document.getElementById("parrafo");
+   let muñeco = document.getElementById("munheco");
+  
+   let textoCifrado = texto
+     .replace(/enter/gi, "e")
+     .replace(/imes/gi, "i")
+     .replace(/ai/gi, "a")
+     .replace(/ober/gi, "o")
+     .replace(/ufat/gi, "u");
+  
+    if (texto.length != 0) {
+       document.getElementById("texto").value = textoCifrado;
+       tituloMensaje.textContent = "Texto desencriptado con éxito.";
+       parrafo.textContent = "";
+       muñeco.src = "./img/desencriptado.jpg";
+    } else {
+      muñeco.src = "./img/Muñeco.png";
+      tituloMensaje.textContent = "Ningun mensaje fue encontrado.";
+      alert ("Debes ingresar algun texto");
+    }
+
+}                  
+               
