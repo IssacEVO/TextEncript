253c8e2d6b1f8828a675cc8be8756ac99e32ae30
Isaac Eduardo Velez Ortega <eduardo_vsiete@yahoo.com>
2023-06-06 13:23:50 -0600

    Diseño de la aplicacion con CSS y responsive designe junto con la funcionalidad de encriptar un texto

---
+++ index.js
@@ -0,0 +1,30 @@
+function encriptar () {
+ let texto = document.getElementById("texto").value;
+ let tituloMensaje = document.getElementById("titulo-mensaje");
+ let parrafo = document.getElementById("parrafo");
+ let muñeco = document.getElementById("munheco");
+
+ let textoCifrado = texto
+   .replace(/e/gi, "enter")
+   .replace(/i/gi, "imes")
+   .replace(/a/gi, "ai")
+   .replace(/o/gi, "ober")
+   .replace(/u/gi, "ufat");
+
+  if texto.length != 0) {
+     document.getElementById("texto").value = textoCifrado;
+     tituloMensaje.textContent = "Texto encriptado con éxito.";
+     parrafo.textContent = "";
+     muñeco.src = "./img/encriptado.jpg";
+  } else {
+    muñeco.src = "./img/Muñeco.png";
+    tituloMensaje.textContent = "Ningun mensaje fue encontrado.";
+    alert ("Debes ingresar algun texto");
+  }
+ 
+
+
+}
+
+                    
+                    
